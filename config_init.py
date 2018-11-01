88719dfd0e3c0bca329855ec8796631a5ded2bf9
james <jamesstewartmiller@gmail.com>
2018-11-01 21:13:53 +0000

    getting deb links in packages.py

--- config_init.py
+++ config_init.py
@@ -2,58 +2,72 @@
 import sys, os
 from pathlib import Path
 import datetime
-from config import Config
+from configobj import ConfigObj
 from dogpile.cache import make_region
 CONFIG_DIR = '.config/kxfed/'
-CONFIG_FILE = 'kxfed.conf'
+CONFIG_FILE = 'kxfed.cfg'
+
+__this__ = sys.modules[__name__]
+
+__this__.cfg = None
+__this__.cache = None
 
 
-class KFConf:
-
-    cfg = None
-    cache = None
-
-    def __init__(self):
+# class KFConf:
+   # def __init__(self):
         # launchpadlib config - not necessary or move to module level / outer scope
-        home = str(Path.home()) + '/'
-        lp_cache_dir = home + ".launchpadlib/api.launchpad.net/cache/"
-        if not os.path.exists(lp_cache_dir):
-            os.mkdir(lp_cache_dir, 0o755)
+        # useful for deleting in case of uninstall?
+home = str(Path.home()) + '/'
+lp_cache_dir = home + ".launchpadlib/api.launchpad.net/cache/"
+if not os.path.exists(lp_cache_dir):
+    os.mkdir(lp_cache_dir, 0o755)
 
-        if KFConf.cfg is None:
-            # also in local function scope. no scope specifier like global is needed
-            config_dir = home + CONFIG_DIR
-            if not os.path.exists(config_dir):
-                os.mkdir(config_dir, 0o755)
-            if not os.path.exists(config_dir + CONFIG_FILE):
-                f = open(config_dir + CONFIG_FILE, "w+")
-                f.write("# kxfed configuration file. Created " + str(datetime.datetime.now()) + "\n")
-                f.write("# constants\nCONFIG_DIR : '" + CONFIG_DIR + "'\nCONFIG_FILE : '" + CONFIG_FILE + "'\n")
-                f.close()
-            f = open(config_dir + CONFIG_FILE, "r+")
-            KFConf.cfg = Config(f)
-            KFConf.cfg.config_dir = config_dir
+if __this__.cfg is None:
+    # also in local function scope. no scope specifier like global is needed
+    config_dir = home + CONFIG_DIR
+    if not os.path.exists(config_dir):
+        os.mkdir(config_dir, 0o755)
 
-        if KFConf.cache is None:
-            # # tree cache built from scratch from the point that the program starts to the point
-            # # that it closes
-            # home = str(Path.home()) + '/'
-            # cache_dir = home + CONFIG_DIR
-            # if not os.path.exists(cache_dir):
-            #     os.mkdir(cache_dir, 0o755)
-            # if not os.path.exists(cache_dir + CACHE_FILE):
-            #     f = open(cache_dir + CACHE_FILE, "w+")
-            #     f.write("# kxfed cache file. Created" + str(datetime.datetime.now()) + '\n')
-            #     f.close()
-            # f = open(cache_dir + CACHE_FILE, "r+")
-            # KFConf.cache = Config(f)
-            # unable to get config to play with make_region.configure_from_config
-            KFConf.cache = make_region().configure(
-                backend=KFConf.cfg.cache.local.backend,
-                expiration_time=KFConf.cfg.cache.local.expiration_time,
-                arguments={
-                    'filename': KFConf.cfg.config_dir + KFConf.cfg.cache.local.arguments.filename
-                }
-            )
+    cache_filename = "kxfed.cache.dbm"
+    if not os.path.exists(config_dir + CONFIG_FILE):
+        __this__.cfg = ConfigObj()
+        __this__.cfg['config']                          = {}
+        __this__.cfg['config']['dir']                   = config_dir
+        __this__.cfg['config']['filename']              = CONFIG_FILE
+        __this__.cfg['cache']                           = {}
+        __this__.cfg['cache']['filename']               = cache_filename
+        __this__.cfg['cache']['backend']                = "dogpile.cache.dbm"
+        __this__.cfg['cache']['enabled']                = "True"
+        __this__.cfg['cache']['expiration_time']        = "604800"
+        __this__.cfg['cache']['arguments']              = {}
+        __this__.cfg['cache']['arguments']['filename']  = config_dir + cache_filename
+        __this__.cfg['installed']                       = {}
+        __this__.cfg['tobeinstalled']                   = {}
+        __this__.cfg['tobeuninstalled']                 = {}
+    else:
+        __this__.cfg = ConfigObj(config_dir + CONFIG_FILE)
 
-KFConf()
+if __this__.cache is None:
+    # # tree cache built from scratch from the point that the program starts to the point
+    # # that it closes
+    # home = str(Path.home()) + '/'
+    # cache_dir = home + CONFIG_DIR
+    # if not os.path.exists(cache_dir):
+    #     os.mkdir(cache_dir, 0o755)
+    # if not os.path.exists(cache_dir + CACHE_FILE):
+    #     f = open(cache_dir + CACHE_FILE, "w+")
+    #     f.write("# kxfed cache file. Created" + str(datetime.datetime.now()) + '\n')
+    #     f.close()
+    # f = open(cache_dir + CACHE_FILE, "r+")
+    # KFConf.cache = Config(f)
+    # unable to get config to play with make_region.configure_from_config
+    # dot notation does not work
+    # __this__.cache = make_region()
+    # __this__.cache.configure_from_config(__this__.cfg, "cache.")
+    __this__.cache = make_region().configure(
+        backend=__this__.cfg['cache']['backend'],
+        expiration_time=int(__this__.cfg['cache']['expiration_time']),
+        arguments={
+            'filename': __this__.cfg['cache']['arguments']['filename']
+        }
+    )
