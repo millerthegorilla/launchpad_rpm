0e4a32bc49868679b4bef9e34a19ffb88d696c2b
james miller <jamesstewartmiller@gmail.com>
2019-03-12 17:42:44 +0000

    working again, now to install.  Switching to new branch to test for dependencies

--- kxfed.py
+++ kxfed.py
@@ -38,17 +38,6 @@ class MainW (QMainWindow, Ui_MainWindow):
         self.pkgs_tableView.horizontalHeader().setStretchLastSection(True)
         self.pkgs_tableView.setStyleSheet("QTableView::QCheckBox::indicator { position : center; }")
 
-        # progress label
-        self.load_label.setVisible(False)
-        self.__movie = QMovie("./assets/loader.gif")
-        self.__movie.start()
-        self.load_label.setMovie(self.__movie)
-
-        # progress bar
-        self.progress_bar.setVisible(False)
-        self._download_total = 0
-        self._download_current = 0
-
         # status bar
         self.label = QLabel()
         self.label.setWordWrap(True)
@@ -59,6 +48,8 @@ class MainW (QMainWindow, Ui_MainWindow):
         self.reconnectBtn.setVisible(False)
         self.reconnectBtn.pressed.connect(self.connect)
 
+        self._movie = QMovie("./assets/loader.gif")
+
         # refresh cache button
         self.btn_refresh_cache.triggered.connect(self.refresh_cache)
 
@@ -67,6 +58,7 @@ class MainW (QMainWindow, Ui_MainWindow):
         self.btn_edit_config.triggered.connect(self.show_prefs)
 
         # signals
+        self.pkg_model.list_filled.connect(self.toggle_pkg_list_loading)
         self.pkg_model.message.connect(self.message_user, type=PyQt5.QtCore.Qt.DirectConnection)
         self.pkg_model.packages.progress_adjusted.connect(self.progress_changed, type=PyQt5.QtCore.Qt.DirectConnection)
         self.pkg_model.packages.progress_label.connect(self.progress_label_change)
@@ -103,28 +95,41 @@ class MainW (QMainWindow, Ui_MainWindow):
                 logging.log(logging.CRITICAL, str(e) + "\n" + traceback.format_exc())
             event.accept()
 
+    def showEvent(self, ev):
+        # progress label
+        self.load_label.setMovie(self._movie)
+        self._movie.start()
+        self.load_label.setVisible(True)
+
+        # progress bar
+        self.progress_bar.setVisible(False)
+        self._download_total = 0
+        self._download_current = 0
+        return QWidget.showEvent(self, ev)
+
     def connect(self):
         try:
             self.pkg_model.packages.connect()
             self.reconnectBtn.setVisible(False)
             self.populate_ppa_combo()
+            self.pkg_model.list_filled.emit()
             self.message_user('Connected to Launchpad')
         except (httplib2.ServerNotFoundError, HTTPError) as e:
             self.reconnectBtn.setVisible(True)
-            self.message_user(str(e) + ' - perhaps check your internet connection. ', 500)
+            self.message_user(str(e) + ' - perhaps check your internet connection. ')
 
     def populate_ppa_combo(self):
         try:
             ppas_link = self.pkg_model.packages.lp_team.ppas_collection_link
-            self.toggle_pkg_list_loading(True)
+            #self.toggle_pkg_list_loading(True)
             self._ppas_json = requests.get(ppas_link).json()
         except requests.HTTPError as e:
-            self.toggle_pkg_list_loading(False)
+            #self.toggle_pkg_list_loading(False)
             logging.log("error", e.strerror)
-            self.message_user(e.strerror, 500)
+            self.message_user(e.strerror)
         for ppa in self._ppas_json['entries']:
             self.ppa_combo.addItem(ppa['displayname'], ppa['name'])
-        self.toggle_pkg_list_loading(False)
+        #self.toggle_pkg_list_loading()
 
     def install_pkgs(self):
         try:
@@ -132,23 +137,33 @@ class MainW (QMainWindow, Ui_MainWindow):
             # TODO do not show the above message again option
         except Exception as e:
             logging.log(logging.CRITICAL, str(e) + "\n" + traceback.format_exc())
-            self.message_user('Critical Error, Exiting', 500, True)
+            self.message_user('Critical Error, Exiting')
 
     def populate_pkgs(self):
         try:
             self.pkg_model.populate_pkg_list(self.ppa_combo.itemData(self.ppa_combo.currentIndex()))
         except Exception as e:
             logging.log(logging.ERROR, str(e))
-            self.message_user(traceback.format_exc(), 500)
+            self.message_user(traceback.format_exc())
 
-    def toggle_pkg_list_loading(self, visible):
-        if visible is True:
-            self.load_label.setVisible(True)
-            self.__movie.start()
+    @pyqtSlot()
+    def toggle_pkg_list_loading(self):
+        self.load_label.setVisible((not self.load_label.isVisible()))
+        if self.load_label.isVisible():
+            self._movie.start()
         else:
-            self.__movie.stop()
-            self.load_label.setVisible(False)
-        self.pkgs_tableView.resizeColumnsToContents()
+            self._movie.stop()
+            self.pkgs_tableView.resizeColumnsToContents()
+
+
+    # def toggle_pkg_list_loading(self, visible):
+    #     if visible is True:
+    #         self.load_label.setVisible(True)
+    #         self.__movie.start()
+    #     else:
+    #         self.__movie.stop()
+    #         self.load_label.setVisible(False)
+    #     self.pkgs_tableView.resizeColumnsToContents()
 
     @pyqtSlot(int, int)
     def progress_changed(self, v, m):
@@ -161,6 +176,7 @@ class MainW (QMainWindow, Ui_MainWindow):
                 self._download_total += m
             if v != 0:
                 self._download_current += v
+            self.statusbar.showMessage("Downloading Packages", 100)
             self.progress_bar.setVisible(True)
             self.progress_bar.setMaximum(self._download_total)
             self.progress_bar.setValue(self._download_current)
@@ -170,7 +186,7 @@ class MainW (QMainWindow, Ui_MainWindow):
         self.progress_label.setText(s)
 
     @pyqtSlot(str)
-    def message_user(self, msg, timeout=0, exit=False):
+    def message_user(self, msg):   # , timeout=0, exit=False):
         self.label.setText(msg)
         # if timeout:
         #     self.label.startTimer(timeout)
@@ -183,7 +199,7 @@ class MainW (QMainWindow, Ui_MainWindow):
 
     @pyqtSlot('PyQt_PyObject')
     def _exception(self, ex):
-        self.message_user(str(ex), 500)
+        self.message_user(str(ex))
         raise type(ex)(traceback.format_exc())
 
     @staticmethod
