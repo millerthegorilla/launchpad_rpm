bf3eebd9910e7cd52f2987cc76c27de9fd2ed835
james stewart miller <jamesstewartmiller@gmail.com>
2019-03-17 18:09:53 +0000

    added logging, getting segfault

--- kxfedmsgsdialog.py
+++ kxfedmsgsdialog.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog
 from kxfedmsgsdialog_ui import Ui_KxfedMsgsDialog
-from messageswindow import MessagesWindow
+import logging
+import logging.handlers
+from kfconf import cfg
 
 
 # TODO - add and sort out logging in MessagesWindow
@@ -10,3 +12,44 @@ class KxfedMsgsDialog(QDialog):
         self.ui = Ui_KxfedMsgsDialog()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.close)
+        self.logger = logging.getLogger(__name__)
+        handler = logging.handlers.RotatingFileHandler(
+            cfg["log_file_path"], maxBytes=2000, backupCount=5)
+        format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(format)
+        self.logger.addHandler(handler)
+
+    def log(self, log_record=None, msg=None):
+        log_msg = ""
+        level = logging.INFO
+        if log_record:
+            if log_record.name:
+                log_msg = "name : " + log_record.name + " : "
+            if log_record.level:
+                log_msg += "level : " + log_record.level + " : "
+            if log_record.pathname:
+                log_msg += "module : " + log_record.pathname + " : "
+            if log_record.lineno:
+                log_msg += "line no : " + log_record.lineno + " : "
+            if log_record.msg:
+                log_msg += "message : " + log_record.msg + " : "
+            if log_record.args:
+                log_msg += "args : " + log_record.args + " : "
+            if log_record.exc_info:
+                log_msg += "exc_info : " + log_record.exc_info + " : "
+            if log_record.func:
+                log_msg += "function : " + log_record.func + " : "
+            if log_record.sinfo:
+                log_msg += "stack info : " + log_record.sinfo
+            level = log_record.level | logging.INFO
+        elif msg:
+            log_msg = msg
+            level = logging.INFO
+
+        if log_msg != "":
+            self.ui.plainTextEdit.appendPlainText(log_msg)
+            self.logger.log(level=level, msg=log_msg)
+        else:
+            self.ui.plainTextEdit.appendPlainText("log function called without info")
+            self.logger.log(level=logging.INFO, msg="no message")
+
